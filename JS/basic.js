13e2be3d210697de5fe4ec44fbece834ee6014c4
adesh sengar <adeshsengar156@gmail.com>
2025-06-25 16:18:35 +0530

    data types

---
+++ JS/basic.js
@@ -0,0 +1,21 @@
+console.log("adesh")
+
+const URL="http://localhost:8080/"
+let accountId="acc123";
+var accountName="adesh sengar";
+accountCity="delhi";
+
+// new method to log the values
+console.table([URL,accountId,accountName,accountCity]);
+
+// URL=23;  // not allowed
+
+let name;
+console.log(name);
+
+// primitive data type.
+// number , string ,boolena, bigInt, null= > standard value, undefined, symbol
+
+// object
+console.log(typeof undefined);  //undefined is of undefined type
+console.log(typeof null);  // null is of object type
