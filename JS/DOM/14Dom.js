d06046146e95452b1d9c98fc39dbe7eaf0d460c3
adesh sengar <adeshsengar156@gmail.com>
2025-07-04 12:29:15 +0530

    added the high order array filters chaining dom manupulation.

---
+++ JS/DOM/14Dom.js
@@ -0,0 +1,24 @@
+
+'use strict'
+// const heading=window.document.getElementById('heading');
+// heading.style.backgroundColor="green";
+// heading.style.padding="15px";
+
+// console.log(heading.innerText);
+
+// document.querySelector('.heading');
+
+// document.querySelector('ul')
+
+
+// one of the usecase is : let say updated data is coming form db , so it should be populated the latest value in the UI.
+
+
+const parent=document.querySelector('.parent');
+// console.log(parent);
+// console.log(parent.children[0].innerHTML);
+
+for(let i=0; i<parent.children; i++){
+    console.log("hello")
+    console.log(parent.children[i].innerHTML)
+}
