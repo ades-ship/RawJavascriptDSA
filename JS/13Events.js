5dd55b537001ee24be09c5eab343adf566e5d76d
adesh sengar <adeshsengar156@gmail.com>
2025-07-10 17:31:25 +0530

    completed the events

--- JS/13Events.js
+++ JS/13Events.js
@@ -6,24 +6,50 @@
 
 // })
 
-const counterDisplay=document.getElementById('counter');
-const increase=document.getElementById("increase");
-const decrease=document.getElementById("decrease");
-const reset=document.getElementById("reset");
+//  ques1.
+// const counterDisplay=document.getElementById('counter');
+// const increase=document.getElementById("increase");
+// const decrease=document.getElementById("decrease");
+// const reset=document.getElementById("reset");
 
-let count=0;
 
-increase.onclick=()=>{
-    count++;
-    counterDisplay.textContent=count;
-}
+// let count=0;
 
-decrease.onclick=()=>{
-    count--;
-    counterDisplay.textContent=count;
-}
+// increase.onclick=()=>{
+//     count++;
+//     counterDisplay.textContent=count;
+// }
 
-reset.onclick=()=>{
-    count=0;
-    counterDisplay.textContent=count;
-}
+// decrease.onclick=()=>{
+//     count--;
+//     counterDisplay.textContent=count;
+// }
+
+// reset.onclick=()=>{
+//     count=0;
+//     counterDisplay.textContent=count;
+// }
+
+// que2
+// most important interview questions
+// event propagation :
+
+  // 1. event capturing 
+  // 
+  // 2.event bubbling ;
+
+  document.getElementById('google').addEventListener('click',function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("google clicked");
+  })
+
+//   ques4
+  document.getElementById("images").addEventListener('click',function(e){
+    console.log(e.target.tagName==='IMG')
+    if(e.target.tagName==='IMG'){
+        console.log(e.target.id);
+        e.target.parentNode.remove();
+    }
+    // console.log(e.target.childNode());
+  })
